fd5ae25b16c43120c6be12fbdd7089f82f41128e
Aastik-Mehta-22 <mehtaaastik12@gmail.com>
2024-10-12 23:21:47 +0530

    add solution

--- binary search 2/searchInrotatedSortedArray.cpp
+++ binary search 2/searchInrotatedSortedArray.cpp
@@ -1,6 +1,53 @@
-#include<iostream>
-using namespace std;
+// Leetcode 33 search in rotated sorted array
 
-int pivot(vector<int> arr, int n){
-    
-}
+#include<iostream>
+#include<bits/stdc++.h>
+
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int start = 0;
+        int n = nums.size();
+
+        int end = n-1;
+
+        
+
+        while(start <= end){
+
+            int mid = start + (end-start)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[start] <= nums[mid]){
+
+                if(target < nums[mid] && target >= nums[start]){
+
+                    end  = mid -1;
+                }
+                else{
+
+                    start = mid + 1;
+                }
+            }
+            else if(nums[mid] <= nums[end]){
+
+                if(target > nums[mid] && target <= nums[end]){
+
+                    start = mid + 1;
+                }
+                else end = mid -1;
+
+
+            }
+            
+        }
+
+        return -1;
+
+    }
+};
